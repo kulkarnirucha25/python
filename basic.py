37b9e29bff46d23c0f833e5e6b9e9a257485a4a8
rucha kulkarni <ruchakulkarni2006@gmail.com>
2026-07-23 10:31:48 +0530

    updated code

--- basic.py
+++ basic.py
@@ -1,1 +1,84 @@
-print("hello world")
+
+# Integer
+num = 10
+print("Integer:", num)
+
+# Float
+price = 99.99
+print("Float:", price)
+
+# Complex
+comp = 3 + 4j
+print("Complex:", comp)
+
+# String
+name = "Rucha"
+print("String:", name)
+
+# List
+arr = [10, 20, 30, 40]
+print("List:", arr)
+
+# Tuple
+t = (1, 2, 3, 4)
+print("Tuple:", t)
+
+# Set
+s = {10, 20, 30, 20}
+print("Set:", s)
+
+# Dictionary
+student = {
+    "Name": "Rucha",
+    "Age": 20,
+    "City": "Kolhapur"
+}
+print("Dictionary:", student)
+
+# IF STATEMENT
+age = 20
+if age >= 18:
+    print("Eligible to vote")
+
+# IF ELSE
+number = 7
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+# IF ELIF ELSE
+marks = 75
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Fail")
+
+# FOR LOOP
+for i in arr:
+    print(i)
+
+# FOR ELSE LOOP
+for i in arr:
+    print(i)
+else:
+    print("Loop completed successfully")
+
+# WHILE LOOP
+count = 1
+while count <= 3:
+    print(count)
+    count += 1
+
+# WHILE ELSE LOOP
+x = 1
+while x <= 3:
+    print(x)
+    x += 1
+else:
+    print("While loop finished")
+
